fbdfc4b9f3ac766f58aba543dda1844d42022f1f
Admin <kavyak6999@gmail.com>
2022-12-18 11:33:08 +0530

    comparison operator

---
+++ operator/operator.js
@@ -0,0 +1,11 @@
+var x = parseInt(prompt("enter the value of x"));
+var y = parseInt(prompt("enter the value of y"));
+//var z = x==y
+var z = x!=y;
+var z = x===y;
+var z = x!==y;
+var z = x>=y;
+var z = x>y;
+var z = x<=y;
+var z = x<y;*/
+alert (z);
